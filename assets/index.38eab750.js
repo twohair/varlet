ad01c7b6ed4791ec60285d8eed56b186596b1068
qytayh <39668309+qytayh@users.noreply.github.com>
2022-02-22 14:15:31 +0000

    Deploying to gh-page from @ varletjs/varlet@f2adc981030481fb218e608342a184c4d1e07588 🚀

--- assets/index.38eab750.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.eef03d0b.js";n.install=function(o){o.component(n.name,n)};
---
+++ assets/index.64da5951.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.c3e43f4b.js";n.install=function(o){o.component(n.name,n)};
