8ec81fe4991b3c17f33d9baefab04bf4fa656148
BeADre <beadre_wang@163.com>
2022-03-16 08:32:09 +0000

    Deploying to gh-page from @ varletjs/varlet@a0c07f66036b0a599117f9c815c4d92ef1186dd0 🚀

---
+++ assets/index.8b8cac6a.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.c9dd91d9.js";n.install=function(o){o.component(n.name,n)};
--- assets/index.b8c414b9.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.a52dc327.js";n.install=function(o){o.component(n.name,n)};
