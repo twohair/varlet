aaf9847960e64df2d1dc4422d78f82470c8b12e0
BeADre <beadre_wang@163.com>
2022-05-05 03:07:51 +0000

    Deploying to gh-page from @ varletjs/varlet@d723e367dd006243d7bf0b2adb0611ae49e19d5f 🚀

--- assets/index.3d32cd38.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.47a461a7.js";n.install=function(o){o.component(n.name,n)};
---
+++ assets/index.dbe18f08.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.e484bb64.js";n.install=function(o){o.component(n.name,n)};
