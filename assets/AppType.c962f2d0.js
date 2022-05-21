2baf665b182b63e3f0004a9a6864f91ac5625092
haoziqaq <357229046@qq.com>
2022-05-21 15:27:09 +0000

    Deploying to gh-page from @ varletjs/varlet@e61482dbd45529630b7082b72abfedc6db5381bc 🚀

--- assets/AppType.c962f2d0.js
+++
@@ -1,1 +0,0 @@
-import{d as p,_ as s,f as t,h as a,J as n}from"./elevation.1daa8134.js";const o=p({name:"AppType"}),_={class:"app-type"};function r(e,c,d,l,f,i){return t(),a("div",_,[n(e.$slots,"default")])}var u=s(o,[["render",r]]);export{u as A};
---
+++ assets/index.decd0f10.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.aa25743b.js";n.install=function(o){o.component(n.name,n)};
